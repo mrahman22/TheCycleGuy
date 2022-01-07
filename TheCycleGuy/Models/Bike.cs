e05afc2072c58dd9ee638e3247dd83c310f81718
Mustabur Rahman <mustabur.rahman@tote.co.uk>
2022-01-07 16:26:10 +0000

    Created a new method called TestRide that check the state of the bike.

--- TheCycleGuy/Models/Bike.cs
+++ TheCycleGuy/Models/Bike.cs
@@ -6,9 +6,56 @@ namespace TheCycleGuy.Models
 {
     public class Bike
     {
-        public DateTime DateLastMaintained = new DateTime(2021, 12, 10);
+        public DateTime DateLastMaintained = new DateTime();
         public BikeTypes Type = BikeTypes.road;
         public Dictionary<Components, Condition> Parts = new Dictionary<Components, Condition>();
+
+
+
+        public string TestRide(Dictionary<Components, Condition> parts)
+        {
+            int count = 0;
+            
+            foreach (KeyValuePair<Components, Condition> value in parts)
+            {
+
+                if (value.Value == Condition.broken)
+                {
+                    return "This bike is broken, I can't ride it like this!";
+                }
+                
+                if (value.Value == Condition.fine || value.Value == Condition.pristine)
+                {
+                    count++;
+                    if (count == 4)
+                    {
+                        return "The bike rides beautifully!";
+                    }
+                }
+
+                if (value.Value == Condition.fragile)
+                {
+                    count++;
+                    if (count == 4)
+                    {
+                        return  "It's a comfy ride!";
+                    }
+                }
+
+            }
+
+            return "";
+        }
+
+        public Bike(DateTime dateTime, BikeTypes type, Dictionary<Components, Condition>parts)
+        {
+            DateLastMaintained = dateTime;
+            Type = type;
+            Parts = parts;
+        }
+        
         
     }
+    
+    
 }
--- TheCycleGuy/Program.cs
+++ TheCycleGuy/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using TheCycleGuy.Enums;
+using TheCycleGuy.Models;
 
 namespace TheCycleGuy
 {
@@ -6,7 +9,18 @@ namespace TheCycleGuy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // Creating Bike 1
+            DateTime dateTime = new DateTime(2021, 10, 5);
+            Dictionary<Components, Condition> parts = new Dictionary<Components, Condition>();
+            parts.Add(Components.tyres, Condition.fine);
+            parts.Add(Components.gears, Condition.fine);
+            parts.Add(Components.frame, Condition.pristine);
+            parts.Add(Components.breaks, Condition.broken);
+            var bike1 = new Bike(dateTime, BikeTypes.road, parts);
+            
+            Console.WriteLine(bike1.TestRide(parts));
+
         }
     }
 }
